e78aeece8542e8ea928f4d5c2f8827588123160c
Max <slobodynuk.777@gmail.com>
2024-01-27 12:43:43 +0200

    handles errors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 const API_KEY = "live_hERv5iu0qrcV4fxbhLLIylPHuCMXOkJv1eXUoUmVn2DhnbrTeoyfJM5hRegHk84P";
 const BASE_URL = "https://api.thecatapi.com/v1";
 
 const breedSelect = document.querySelector(".breed-select");
 const catInfo = document.querySelector(".cat-info");
+const loader = document.querySelector(".loader");
+const error = document.querySelector(".error");
 
+showLoader()
 
 function fetchBreeds() {
     return fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`)
@@ -18,8 +23,12 @@ function fetchBreeds() {
 
 fetchBreeds().then((data) => {
     createMarkupOption(data)
+    hideLoader()
+}).catch((err) => {
+    Notify.failure(error.textContent);
+    hideLoader()
+})
 
-}).catch(err => console.log(err))
 
 function createMarkupOption(breeds) {
     const option = breeds.map(breed => 
@@ -27,6 +36,7 @@ function createMarkupOption(breeds) {
         .join('')
     breedSelect.innerHTML = option;
 }
+
 
 function fetchCatByBreed(breedId) {
     return fetch(`${BASE_URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`)
@@ -40,15 +50,19 @@ function fetchCatByBreed(breedId) {
 }
 
 
-
-
 breedSelect.addEventListener('change', () => {
     const idElement = breedSelect.options[breedSelect.selectedIndex].value;
+    hideInfo()
+    showLoader()
 
     fetchCatByBreed(idElement).then((data) => {
         createMarkupBreedInformation(data);
-    
-    }).catch(err => console.log(err))
+        hideLoader()
+        showInfo()
+    }).catch((err) => {
+        Notify.failure(error.textContent);
+        hideLoader()
+    })
 })
 
 
@@ -62,4 +76,21 @@ function createMarkupBreedInformation(breeds) {
         </ul>`).join('');
 
         catInfo.innerHTML = markup;
+}
+
+
+function showLoader() {
+    loader.style.display = 'block'
+}
+
+function hideLoader() {
+    loader.style.display = 'none'
+}
+
+function showInfo() {
+    catInfo.style.display ='block'
+}
+
+function hideInfo() {
+    catInfo.style.display ='none'
 }
